e0b4f43bb8e745d8a17a884a402eeb7b90679ef1
Vladyslav Livandovskyi <nonameb281@gmail.com>
2026-08-16 22:24:48 +0200

    (#6) read & write functions to take files

--- src/camougg/core/steg/steg_read.py
+++ src/camougg/core/steg/steg_read.py
@@ -20,6 +20,79 @@ class StegReader:
             if num_pixels < 44:
                 raise ValueError("Image is too small to read your message.")
 
+            flat_pixels = pixel_array.reshape(-1, 3)
+            shifts = np.arange(8, dtype=np.uint8)
+            salt_pixels = flat_pixels[:44]
+            salt_pixels_shifted = (salt_pixels[..., np.newaxis] >> shifts) & 1
+            lsb_salt_pixels = salt_pixels_shifted[..., 0].flatten()[:128]
+            salt = np.packbits(lsb_salt_pixels).tobytes()
+
+            prp = self.generator.hash_password(password, num_pixels - 44, salt)
+
+            prp_shifted = [i + 44 for i in prp]
+            chosen_pixels = flat_pixels[prp_shifted]
+            bits = (chosen_pixels[..., np.newaxis] >> shifts) & 1
+            bitstream = bits[..., 0].flatten()
+
+            weights = 2 ** np.arange(8, dtype=np.uint16)
+            pos = 0
+
+            def read_next_byte():
+                nonlocal pos
+                if pos + 8 > bitstream.size:
+                    raise ValueError("Invalid password or corrupted image")
+                byte_bits = bitstream[pos:pos + 8]
+                pos += 8
+                return int(np.dot(byte_bits, weights))
+
+            if read_next_byte() != ord('<'):
+                raise ValueError("Invalid password or corrupted image")
+
+            length_bytes = bytearray()
+            while True:
+                b = read_next_byte()
+                if b == ord('|'):
+                    break
+                length_bytes.append(b)
+            try:
+                file_size_bytes = int(length_bytes.decode('ascii'))
+            except ValueError:
+                raise ValueError("Invalid password or corrupted image")
+
+            total_bits = file_size_bytes * 8
+
+            filename_bytes = bytearray()
+            while True:
+                b = read_next_byte()
+                if b == ord('>'):
+                    break
+                filename_bytes.append(b)
+
+            filename = filename_bytes.decode('utf-8', errors='replace')
+
+            if total_bits == 0:
+                return b"", filename
+
+            if pos + total_bits > bitstream.size:
+                raise ValueError("Invalid password or corrupted image")
+            msg_bits = bitstream[pos:pos + total_bits].reshape(-1, 8)
+            byte_values = msg_bits.dot(weights).astype(np.uint8)
+            return bytes(byte_values), filename
+        except (ValueError, OSError, IOError) as e:
+            if "Invalid password" in str(e):
+                raise
+            raise ValueError("Invalid password or corrupted image")
+
+
+    """    def steg_read(self, img_path, password):
+        try:
+            img = Image.open(img_path)
+            img_rgb = img.convert("RGB")
+            pixel_array = np.array(img_rgb)
+            num_pixels = self.get_num_pixels(img_path)
+            if num_pixels < 44:
+                raise ValueError("Image is too small to read your message.")
+
 
             flat_pixels = pixel_array.reshape(-1, 3)
             shifts = np.arange(8, dtype=np.uint8)
@@ -79,4 +152,4 @@ class StegReader:
         except (ValueError, OSError, IOError) as e:
             if "Invalid password" in str(e):
                 raise
-            raise ValueError("Invalid password or corrupted image")
+            raise ValueError("Invalid password or corrupted image")"""
--- src/camougg/core/steg/steg_write.py
+++ src/camougg/core/steg/steg_write.py
@@ -11,13 +11,15 @@ class StegWriter:
         width, height = Image.open(filepath).size
         return width * height
 
-    def steg_write(self, img_path, password, message, output_path="output/output1.png"):
-        if not message:
+    def steg_write(self, img_path, password, data: bytes, filename: str, output_path):
+        if not data:
             raise ValueError("Message cannot be empty")
 
-        message = "<{}>{}".format(len(message), message)
-        byte_array = np.frombuffer(message.encode("utf-8"), dtype=np.uint8)
+        header = "<{}|{}>".format(len(data), filename)
         num_pixels = self.get_num_pixels(img_path)
+        header_bytes = header.encode("utf-8")
+        full_payload = header_bytes + data
+        byte_array = np.frombuffer(full_payload, dtype=np.uint8)
 
         if num_pixels < 44:
             raise ValueError("Image is too small to hide your message.")
@@ -29,7 +31,7 @@ class StegWriter:
             raise ValueError("Message length exceeds image capacity")
 
         salt = os.urandom(16)
-        prp = self.generator.hash_password(password, num_pixels - 44,salt)
+        prp = self.generator.hash_password(password, num_pixels - 44, salt)
         prp_shifted = [i + 44 for i in prp]
 
         try:
@@ -44,15 +46,18 @@ class StegWriter:
         flat_pixels = pixel_array.reshape(-1, 3)
 
         salt_pixels = flat_pixels[:44]
-        salt_pixels_shifted = (salt_pixels[..., np.newaxis] >> shifts) & 1 # [[255, 230, 211]] -> [[11111111, 0100010101 ,10111010]]
-        salt_bytes = np.unpackbits(np.frombuffer(salt, dtype=np.uint8)) # [0,1,1,...1]
-        flat_lsb = salt_pixels_shifted[..., 0].flatten() # Take the least significant bit for every color
-        flat_lsb[:128] = salt_bytes # replace 1st 128 bits with salt bites
-        salt_pixels_shifted[..., 0] = flat_lsb.reshape(44, 3) # Reshape to 3D Matrix
-        reconstructed_salt_pixels = np.sum(salt_pixels_shifted << shifts, axis=-1).astype(np.uint8) # Make colors from bits
-        flat_pixels[:44] = reconstructed_salt_pixels # replace original pixels with "salted" pixels
+        salt_pixels_shifted = (salt_pixels[
+                                   ..., np.newaxis] >> shifts) & 1  # [[255, 230, 211]] -> [[11111111, 0100010101 ,10111010]]
+        salt_bytes = np.unpackbits(np.frombuffer(salt, dtype=np.uint8))  # [0,1,1,...1]
+        flat_lsb = salt_pixels_shifted[..., 0].flatten()  # Take the least significant bit for every color
+        flat_lsb[:128] = salt_bytes  # replace 1st 128 bits with salt bites
+        salt_pixels_shifted[..., 0] = flat_lsb.reshape(44, 3)  # Reshape to 3D Matrix
+        reconstructed_salt_pixels = np.sum(salt_pixels_shifted << shifts, axis=-1).astype(
+            np.uint8)  # Make colors from bits
+        flat_pixels[:44] = reconstructed_salt_pixels  # replace original pixels with "salted" pixels
 
-        prp = prp_shifted[:message_bits.size]
+        num_pixels_needed = -(-message_bits.size // 3)
+        prp = prp_shifted[:num_pixels_needed]
 
         chosen_pixels = flat_pixels[prp]
         bits = (chosen_pixels[..., np.newaxis] >> shifts) & 1
